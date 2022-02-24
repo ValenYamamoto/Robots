1009e7900f067b71f17fb21f4fda902cb67b7625
Valen Yamamoto <valenryamamoto@gmail.com>
2022-02-24 07:12:24 -0800

    Additional logs + scripts

---
+++ src/train_fetch/scripts/move_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import rospy
+import os
+import utils
+
+
+
+if __name__ == "__main__":
+    utils.reset_world()
+
---
+++ src/train_fetch/scripts/time_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import rospy
+import datetime
+
+rospy.init_node("time" )
+
+d = rospy.Duration( 5.0 )
+rospy.sleep( d )
+a = datetime.datetime.now()
+begin = rospy.Time.now()
+rospy.sleep( d )
+now = rospy.Time.now()
+b = datetime.datetime.now()
+
+print( begin.to_sec(), begin.nsecs )
+print( now.secs, now.nsecs )
+duration = now-begin
+print( duration.to_sec(), duration.nsecs )
+c=b-a
+print( c )
